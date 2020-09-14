0b1f39639d7c2775ab8595b0d3530a64f526e5ee
Администратор <@DESKTOP-PHORFLN>
2020-09-14 19:03:13 +0300

    123

---
+++ Task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
+        static void Main(string[] args)
+        {
+            watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            static void watcher_Changed(object sender, FileSystemEventArgs e)
+            {
+                Console.WriteLine("Changed: {0}", e.FullPath);
+            }
+        }
+    }
+}
