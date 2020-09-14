bedfe795c165e5495f3cfb3602b06737fe1cdca6
Администратор <@DESKTOP-PHORFLN>
2020-09-14 17:56:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("File:{0}", file.FullName);
+            }
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                ShowDirectory(subDir);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory);
+            ShowDirectory(dir);
+        }
+    }
+}
