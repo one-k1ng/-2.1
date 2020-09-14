fa21acebd3f63328917bb9e6c6de1c4d1e6f99fe
Администратор <@DESKTOP-PHORFLN>
2020-09-14 19:07:23 +0300

    123

--- Task2.cs
+++ Task2.cs
@@ -6,12 +6,16 @@ namespace Task1
     class Program
     {
         static FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
+
+        static bool EnabhngRaisingEvents { get; private set; }
+
         static void Main(string[] args)
         {
             watcher.Filter = "*.ini";
             watcher.IncludeSubdirectories = true;
             watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
             watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            EnabhngRaisingEvents = true;
             static void watcher_Changed(object sender, FileSystemEventArgs e)
             {
                 Console.WriteLine("Changed: {0}", e.FullPath);
